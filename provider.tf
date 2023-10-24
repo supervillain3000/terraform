bb47bd5f7e47279112300c44e48f47d0c9fe01aa
Ivan Sukhanov <1sukhanovbusiness@gmail.com>
2023-10-24 14:41:31 +0600

    initial commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 0.14.0"
+  required_providers {
+    openstack = {
+      source  = "terraform-provider-openstack/openstack"
+      version = "~> 1.51.1"
+    }
+  }
+}
+
+provider "openstack" {
+  region = "kz-ala-1"
+}
